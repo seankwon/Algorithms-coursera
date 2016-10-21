b18e0016e24799c858d4eeb7d50588897daf3a29
Sean Kwon <Sean Kwon>
2016-10-21 10:18:43 -0400

    first implementation of percolation class

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -1,52 +1,82 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.util.Arrays;
 
 public class Percolation {
     WeightedQuickUnionUF uf;
-    int[][] sites;
-    int N;
+    public int[][] sites;
+    public int N;
 
     public Percolation(int n) {
-        N = n;
-        uf = new WeightedQuickUnionUF(n+2);
-        sites = new int[n][n];
-        for (int[] a : sites) Array.fill(a, 0);
-    }
-    public void open(int row, int col) {
-        // col * row uf.union(row, row+1)
-        // uf.union(row, )
-        if (row < 1 || row > N || col < 1 || col > N)
-            throw new IllegalArgumentException();
-
-        sites[row][col] = 1;
-
-        if (row != 1)
-            uf.union(row-1, row);
-
-        if (row != N)
-            uf.union(row, row+1);
-
-        if (col != 1)
-            uf.union(col-1, col);
-
-        if (col != N)
-            uf.union(col, col+1);
+        this.N = n;
+        this.uf = new WeightedQuickUnionUF(n*n+2);
+        this.sites = new int[n][n];
+        for (int[] a : sites) Arrays.fill(a, 0);
     }
 
     public boolean isOpen(int row, int col) {
-        return true;
+        if (row < 1 || row > N || col < 1 || col > N)
+            throw new IllegalArgumentException();
+        return sites[row-1][col-1] == 1;
     }
 
     public boolean isFull(int row, int col) {
-        return true;
+        if (row < 1 || row > N || col < 1 || col > N)
+            throw new IllegalArgumentException();
+        return !isOpen(row, col);
     }
 
+    public void open(int row, int col) {
+        if (row < 1 || row > N || col < 1 || col > N)
+            throw new IllegalArgumentException();
+
+        if (isOpen(row, col))
+            return;
+
+        if (row != 1)
+            uf.union((N*col)+row-1, (N*col)+row-2);
+
+        if (row != N)
+            uf.union((N*col)+row-1, (N*col)+row);
+
+        if (col != 1)
+            uf.union((N*(col-1))+row, (N*(col-2))+row);
+
+        if (col != N)
+            uf.union((N*(col-1))+row, (N*col)+row);
+
+        //Union virtual vertex
+        if (col == 1)
+            uf.union(0, (N*col)+row);
+        else if (col == N)
+            uf.union(N+1, (N*col)+row);
+
+        sites[row-1][col-1] = 1;
+    }
+
+
     public boolean percolates() {
-        return true;
+        return uf.find(0) == uf.find(N+1);
     }
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Percolation p = new Percolation(3);
+        p.open(1, 1);
+
+        if (p.isOpen(1, 1)) {
+            System.out.println("Test 1 Pass");
+        }
+
+        if (!p.isFull(1,1)) {
+            System.out.println("Test 2 Pass");
+        }
+
+        p.open(1, 2);
+        p.open(1, 3);
+
+        if (p.percolates()) {
+            System.out.println("Test 3 Pass");
+        }
     }
 }
